0a64534097c7920927fa494ea58ef5e179933378
siddharth sharma <siddharth.sharma@comprotechnologies.com>
2020-06-10 16:29:20 +0530

    detail page n minor fixes

--- src/app/task-detail/task-detail.component.ts
+++ src/app/task-detail/task-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TaskService } from "../task.service";
+import { Task } from '../task';
+import { Location } from "@angular/common";
+import { ActivatedRoute, Router} from '@angular/router';
 @Component({
   selector: 'app-task-detail',
   templateUrl: './task-detail.component.html',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskDetailComponent implements OnInit {
 
-  constructor() { }
+  task: Task;
+  showLoading: Boolean;
+  constructor(private taskService: TaskService, private location: Location, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    let _id = this.route.snapshot.params.id
+    this.getTask(_id);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  getTask(_id: String): void {
+    this.showLoading = true;
+    this.taskService.getTask(_id)
+      .subscribe(
+        (data) => {
+          this.task = data;
+          this.showLoading = false;
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+
+  editTask(_id: String): void {
+    this.router.navigateByUrl('/task/edit/'+ _id)
   }
 
 }
